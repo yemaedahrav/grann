92a11a173695c244fd24e535f388fb570caa9715
ravishankar <rakri@microsoft.com>
2024-12-26 20:04:10 -0800

    Feat: add utility script

---
+++ run-script.sh
@@ -0,0 +1,3 @@
+cd build/tests
+./build_hnsw float l2 /nvmessd1/fbv4/prec1M.fbin /nvmessd1/fbv4/avarhade/grann/hnsw_prec1M_L100_R64_a1.2 64 100 1.2 0 0.1 48
+./search_hnsw float l2 /nvmessd1/fbv4/avarhade/grann/hnsw_prec1M_L100_R64_a1.2 2 48 /nvmessd1/fbv4/queries.fbin /nvmessd1/fbv4/prec1M_gt100.bin 25 hnsw_results 50 75 100 150 200
